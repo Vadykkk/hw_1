142b8cb6c4b3e1f3e09e2d9a51d5a79b4b1190ea
Dr.Kentys <svs_1988@list.ru>
2023-06-12 15:42:52 +0300

    домашнне задание_1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > b) {
+            Console.WriteLine("Максимальное число: " + a);
+            Console.WriteLine("Минимальное число: " + b);
+        } else {
+            Console.WriteLine("Максимальное число: " + b);
+            Console.WriteLine("Минимальное число: " + a);
+        }
+    }
+}
